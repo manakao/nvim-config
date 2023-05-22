34a13de3c30ccfdbfcae1f6bfc5cdf860baa8334
Farziev Amin <farziev@gmail.com>
2023-05-23 04:08:17 +0700

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 require('plugins/mason')
 require('plugins/mason-lsp')
 require('plugins/mason-null-ls')
+require('plugins/null-ls')
 require('plugins/lsp')
 require('plugins/lsp-saga')
 require('plugins/treesitter')
--- lua/setup.lua
+++ lua/setup.lua
@@ -1,14 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -17,45 +17,46 @@ local opts = {}
 
 -- https://github.com/folke/lazy.nvim#-plugin-spec
 local plugins = {
-    { 'navarasu/onedark.nvim', lazy = true },
-    { 'nvim-tree/nvim-web-devicons' },
-    { 'numToStr/Comment.nvim' },
+	{ "navarasu/onedark.nvim", lazy = true },
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "numToStr/Comment.nvim" },
 
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-    },
+	{ "nvim-lua/plenary.nvim" },
 
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" }
-    },
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-    { 'neovim/nvim-lspconfig' },
-    {
-        "glepnir/lspsaga.nvim",
-        event = "LspAttach",
-        dependencies = {
-            { "nvim-tree/nvim-web-devicons" },
-            {"nvim-treesitter/nvim-treesitter"}
-        }
-    },
-    { 'jose-elias-alvarez/null-ls.nvim' },
-    { 'jayp0521/mason-null-ls.nvim' },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+	},
 
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'rafamadriz/friendly-snippets' },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{
+		"glepnir/lspsaga.nvim",
+		event = "LspAttach",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	},
+	{ "jose-elias-alvarez/null-ls.nvim" },
+	{ "jayp0521/mason-null-ls.nvim" },
+
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "rafamadriz/friendly-snippets" },
 }
 
-require('lazy').setup(plugins, opts);
-
+require("lazy").setup(plugins, opts)
